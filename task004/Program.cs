ce22a87c13553ae5628526bf9a0e931088770901
AlexCh <stovertikal@mail.ru>
2022-08-09 17:01:15 +0700

    add task 004

---
+++ task004/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+    Напишите программу, которая принимает на вход число и проверяет, 
+    кратно ли оно одновременно 7 и 23. 
+    14 -> нет 
+    46 -> нет 
+    161 -> да
+*/
+
+Console.Clear();
+
+int MultipleNumber(int number)
+{
+    int number1 = 7;
+    int number2 = 23;
+    int result1 = (number % number1);
+    int result2 = (number % number2);
+    if (result1 == 0 && result2 == 0)
+    {
+        return 1;
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+    }
+    return 0;
+}
+
+
+void PrintResult(int result, int number)
+{
+    if (result == 1)
+    {
+        Console.WriteLine($"Число {number} кратно числу 23 и числу 7");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не кратно числу 23 и числу 7");
+    }
+}
+
+Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = MultipleNumber(number);
+
+PrintResult(result, number);
+
+
+
